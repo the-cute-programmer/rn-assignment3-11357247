b201190620963cc59f10446945c39c837029610a
Dede <154935734+rhoda-lee@users.noreply.github.com>
2024-05-31 23:14:13 +0000

    first component comit

--- rn-assignment3/components/TitleFrame.js
+++ rn-assignment3/components/TitleFrame.js
@@ -2,12 +2,39 @@ import {View, Text, StyleSheet, Image} from 'react-native';
 
 export default function App(){
     return(
-        <View style={StyleSheet.frame}>
+        <View style={styles.frame}>
             <View>
-                <Text style={StyleSheet.titleText}>
+                <Text style={styles.titleText}>
                     Hello, Devs
                 </Text>
+                <Text>14 tasks today</Text>
+            </View>
+            <View style={styles.profileIcon}>
+                <Image source={require("../assets/image.png")}/>
             </View>
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    frame:{
+        display: "flex",
+        justifyContent: "space-between",
+        alignContent: "center",
+        flexDirection: "row",
+        width: "100%",
+        borderColor: "black",
+    },
+    titleText:{
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    profileIcon:{
+        backgroundColor: "white",
+        borderRadius: 50,
+        width: 50,
+        height: 50,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
