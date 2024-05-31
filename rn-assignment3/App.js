abacf66e3dd9e6b2e4e7d0f4cfe7e27550ad5eb3
Dede <154935734+rhoda-lee@users.noreply.github.com>
2024-05-31 23:35:23 +0000

    updated assets

--- rn-assignment3/App.js
+++ rn-assignment3/App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+import TitleFrame from "./components/TitleFrame"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View></View>
+    <ScrollView> 
+      <View style={styles.container}>
+        <TitleFrame/>
+        <SearchBar/>
+        <Categories/>
+        <OngoingTask/>
       <StatusBar style="auto" />
     </View>
+    </ScrollView>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    flex: 1,
+    backgroundColor: '#f7f0e8',
+    paddingTop: 52, 
+    padding: 20, 
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
   },
 });
