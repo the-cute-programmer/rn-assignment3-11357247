4fe2037379c100b601c8a9a6c3e4bfa737928080
Dede <154935734+rhoda-lee@users.noreply.github.com>
2024-05-31 22:37:21 +0000

    created components

--- rn-assignment3/App.js
+++ rn-assignment3/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View></View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +12,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
---
+++ rn-assignment3/components/TitleFrame.js
@@ -0,0 +1,13 @@
+import {View, Text, StyleSheet, Image} from 'react-native';
+
+export default function App(){
+    return(
+        <View style={StyleSheet.frame}>
+            <View>
+                <Text style={StyleSheet.titleText}>
+                    Hello, Devs
+                </Text>
+            </View>
+        </View>
+    )
+}
